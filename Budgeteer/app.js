fdeab1a4abc12f6cd7dc2141fa1e504912756330
HomeMac <homemac@HomeMacs-MacBook-Air.local>
2013-09-25 21:47:33 -0500

    started work on creating the models

--- Budgeteer/app.js
+++ Budgeteer/app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
+
 
 var app = express();
 
